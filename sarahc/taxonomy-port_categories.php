5685a702e811d467a0f491ab8eb1909e3b31f78b
Brian Norris <bnorris@marlinco.com>
2017-04-30 18:46:42 -0500

    PHP layout changes

--- sarahc/taxonomy-port_categories.php
+++ sarahc/taxonomy-port_categories.php
@@ -38,11 +38,15 @@ get_header(); ?>
 		$the_query = new WP_Query( $args );
 
 	?>
-<div >
-			<ul class="small-block-grid-2 medium-block-grid-3 large-block-grid-4" id="myGrid">
+	<script>
+		window.ports = [];
+	</script>
+<div>
+			<ul class="port-grid" id="myGrid">
 				<?php
 				// The Loop
-				//$i = 0;
+				$i = 0;
+				$stack = array();
 				while ( $the_query->have_posts() ) : $the_query->the_post();
 				?>
 				<li>
@@ -52,21 +56,28 @@ get_header(); ?>
 					//$thumb_id = get_post_thumbnail_id($post->id);
 					//$alt = get_post_meta($thumb_id, '_wp_attachment_image_alt', true);
 					//echo $alt;
+
 					$id = $post->ID;
 					$url = base64_encode(get_permalink( $id ));
 					$urlNE = get_permalink( $id );
+					array_push($stack, $urlNE);
 				?>
+				<script>
+					window.ports.push('<?php echo $urlNE ?>');
+				</script>
 					<!--<div class="pieceLink" v-on:click="loadPort('<?php echo $url ?>')">
 						<img src="<?php echo $image[0]; ?>" width="<?php echo $image[1]; ?>" height="<?php echo $image[2]; ?>" alt="<?php //the_title(); ?>" class="projectThumb">
 						<div class="overlay"><?php //the_title(); ?></div>
 					</div>-->
-					<a href="<?php echo $urlNE ?>" class="modaal-ajax pieceLink">
+					<a href="<?php echo $urlNE ?>" class="modaal-ajax pieceLink" data-port="<?php echo $i ?>">
 						<img src="<?php echo $image[0]; ?>" width="<?php echo $image[1]; ?>" height="<?php echo $image[2]; ?>" alt="<?php //the_title(); ?>" class="projectThumb">
 						<div class="overlay">
 							<h4><?php the_title(); ?></h4>
 						</div>
 					</a>
+
 				<?php
+					$i++;
 					endif;
 				?>
 
@@ -85,6 +96,15 @@ get_header(); ?>
 		<!--<div id="myModal" class="reveal-modal medium" data-reveal></div>-->
 		<!--<a v-on:click="hidePort" v-show="activated == true" class="cancel"><span><img src="<?php echo get_template_directory_uri(); ?>/img/x.png"></span></a>-->
 		<!--<div id="port-container" v-html="content"></div>-->
+		<div id="port-container">
+			<button type="button" name="close" class="close">X</button>
+			<button type="button" name="previous" class="previous direction" data-page="" data-port="">P</button>
+			<div class="spinner" id="ajax-loader"><div class="bounce1"></div><div class="bounce2"></div><div class="bounce3"></div></div>
+			<div id="port-container-content">
+
+			</div>
+			<button type="button" name="next" class="next direction" data-page="" data-port="">N</button>
+		</div>
 	</section><!-- #primary -->
 
 
--- sarahc/tmplt-home.php
+++ sarahc/tmplt-home.php
@@ -21,32 +21,45 @@ get_header(); ?>
 		$the_query = new WP_Query( $args );
 
 	?>
-
+	<script>
+		window.ports = [];
+	</script>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 		<div class="row">
 		<div class="small-12 columns">
-			<ul class="small-block-grid-2 medium-block-grid-3 large-block-grid-4" id="myGrid">
+			<ul class="port-grid" id="myGrid">
 				<?php
 				// The Loop
-				//$i = 0;
+				$i = 0;
+				$stack = array();
 				while ( $the_query->have_posts() ) : $the_query->the_post();
 				?>
 				<li>
-					
-				<?php 
+
+				<?php
 					if (has_post_thumbnail( $post->ID ) ):
-					$image = wp_get_attachment_image_src(get_post_thumbnail_id( $post->ID ), 'medium' ); 
+					$image = wp_get_attachment_image_src(get_post_thumbnail_id( $post->ID ), 'medium' );
 					//$thumb_id = get_post_thumbnail_id($post->id);
 					//$alt = get_post_meta($thumb_id, '_wp_attachment_image_alt', true);
 					//echo $alt;
+
+					$id = $post->ID;
+					$url = base64_encode(get_permalink( $id ));
+					$urlNE = get_permalink( $id );
+					array_push($stack, $urlNE);
 				?>
-					<a href="<?php the_permalink(); ?>" class="pieceLink" data-reveal-id="myModal"><div class="overlay">
-						<img src="<?php echo $image[0]; ?>" width="<?php echo $image[1]; ?>" height="<?php echo $image[2]; ?>" alt="<?php //the_title(); ?>" data-adaptive-background='1' class="projectThumb">	
-						<div class="overlay"><?php //the_title(); ?></div>
-					</a>
-				<?php 
-					endif; 
+				<script>
+					window.ports.push('<?php echo $urlNE ?>');
+				</script>
+				<a href="<?php echo $urlNE ?>" class="modaal-ajax pieceLink" data-port="<?php echo $i ?>">
+					<img src="<?php echo $image[0]; ?>" width="<?php echo $image[1]; ?>" height="<?php echo $image[2]; ?>" alt="<?php //the_title(); ?>" class="projectThumb">
+					<div class="overlay">
+						<h4><?php the_title(); ?></h4>
+					</div>
+				</a>
+				<?php
+					endif;
 				?>
 				</li>
 				<?php
@@ -54,12 +67,21 @@ get_header(); ?>
 				endwhile;
 				?>
 			</ul>
-			
+
 			<div id="myModal" class="reveal-modal medium" data-reveal></div>
-			
+
 		</div>
 		</div>
 		</main><!-- #main -->
+		<div id="port-container">
+			<button type="button" name="close" class="close">X</button>
+			<button type="button" name="previous" class="previous direction" data-page="" data-port="">P</button>
+			<div class="spinner" id="ajax-loader"><div class="bounce1"></div><div class="bounce2"></div><div class="bounce3"></div></div>
+			<div id="port-container-content">
+
+			</div>
+			<button type="button" name="next" class="next direction" data-page="" data-port="">N</button>
+		</div>
 	</div><!-- #primary -->
 
 <?php //get_sidebar(); ?>
