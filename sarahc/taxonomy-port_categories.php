5b8d43944b1771cbc3b6609c015eb6daf8d88fcd
Brian Norris <bnorris@marlinnetwork.com>
2017-08-24 20:41:04 -0500

    portfolio finishing touches

--- sarahc/taxonomy-port_categories.php
+++ sarahc/taxonomy-port_categories.php
@@ -97,12 +97,45 @@ get_header(); ?>
 		<!--<a v-on:click="hidePort" v-show="activated == true" class="cancel"><span><img src="<?php echo get_template_directory_uri(); ?>/img/x.png"></span></a>-->
 		<!--<div id="port-container" v-html="content"></div>-->
 		<div id="port-container">
-			<button type="button" name="close" class="close">X</button>
-			<button type="button" name="previous" class="previous direction" data-page="" data-port="">P</button>
+			<button type="button" name="close" class="button close">
+				<svg width="14px" height="14px" viewBox="0 0 14 14" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+				    <!-- Generator: Sketch 46.2 (44496) - http://www.bohemiancoding.com/sketch -->
+				    <desc>Created with Sketch.</desc>
+				    <defs></defs>
+				    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+				        <g id="Close" transform="translate(-51.000000, -47.000000)" fill="#D8D8D8">
+				            <path d="M57.7175144,51.5961941 L53.1213203,47 L51,49.1213203 L55.5961941,53.7175144 L51,58.3137085 L53.1213203,60.4350288 L57.7175144,55.8388348 L62.3137085,60.4350288 L64.4350288,58.3137085 L59.8388348,53.7175144 L64.4350288,49.1213203 L62.3137085,47 L57.7175144,51.5961941 Z" id="Combined-Shape"></path>
+				        </g>
+				    </g>
+				</svg>
+			</button>
+			<button type="button" name="previous" class="button previous direction" data-page="" data-port="">
+				<svg width="9px" height="14px" viewBox="0 0 9 14" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+				    <!-- Generator: Sketch 46.2 (44496) - http://www.bohemiancoding.com/sketch -->
+				    <desc>Created with Sketch.</desc>
+				    <defs></defs>
+				    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+				        <g id="Chevrons" transform="translate(-55.000000, -5.000000)" fill="#D8D8D8">
+				            <path d="M59.4194174,16.1369318 L61.7175144,18.4350288 L63.8388348,16.3137085 L59.2426407,11.7175144 L63.8388348,7.12132034 L61.7175144,5 L55,11.7175144 L59.4194174,16.1369318 Z" id="Combined-Shape"></path>
+				        </g>
+				    </g>
+				</svg>
+			</button>
 			<div id="port-container-content">
 			</div>
 			<div class="spinner" id="ajax-loader"><div class="bounce1"></div><div class="bounce2"></div><div class="bounce3"></div></div>
-			<button type="button" name="next" class="next direction" data-page="" data-port="">N</button>
+			<button type="button" name="next" class="button next direction" data-page="" data-port="">
+				<svg width="9px" height="14px" viewBox="0 0 9 14" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+				    <!-- Generator: Sketch 46.2 (44496) - http://www.bohemiancoding.com/sketch -->
+				    <desc>Created with Sketch.</desc>
+				    <defs></defs>
+				    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+				        <g id="Chevrons" transform="translate(-55.000000, -25.000000)" fill="#D8D8D8">
+				            <path d="M59.4194174,36.1369318 L61.7175144,38.4350288 L63.8388348,36.3137085 L59.2426407,31.7175144 L63.8388348,27.1213203 L61.7175144,25 L55,31.7175144 L59.4194174,36.1369318 Z" id="Combined-Shape-Copy" transform="translate(59.419417, 31.717514) scale(-1, 1) translate(-59.419417, -31.717514) "></path>
+				        </g>
+				    </g>
+				</svg>
+			</button>
 		</div>
 	</section><!-- #primary -->
 
